f785baad8199041605d69651f67d256c66846fea
Zak Smith <zsmith3.dev@gmail.com>
2021-06-29 09:40:08 +0100

    Reverse order on multiples of 17
    Refactor to use array of words, count up to 1000 to show examples

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-for (let i = 1; i <= 200; i++ ) {
-	let out = "";
-	if (i % 3 == 0) out += "Fizz";
-	if (i % 5 == 0) out += "Buzz";
-	if (i % 7 == 0) out += "Bang";
-	if (i % 11 == 0) out = "Bong";
-
+for (let i = 1; i <= 1000; i++ ) {
+	let out: string[] = [];
+	if (i % 3 == 0) out.push("Fizz");
+	if (i % 5 == 0) out.push("Buzz");
+	if (i % 7 == 0) out.push("Bang");
+	if (i % 11 == 0) out = ["Bong"];
 	if (i % 13 == 0) {
-		let j = out.indexOf("B");
+		let j = out.findIndex(s => s[0] === "B");
 		if (j === -1) j = out.length;
-		out = out.slice(0, j) + "Fezz" + out.slice(j);
+		out.splice(j, 0, "Fezz");
 	}
+	if (i % 17 == 0) out.reverse();
 
-	if (out) console.log(out);
+	if (out.length) console.log(out.join(""));
 	else console.log(i);
 }
