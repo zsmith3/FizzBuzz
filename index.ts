0c45cc9654ff8eb166d34f205d48e5080047ffff
Zak Smith <zsmith3.dev@gmail.com>
2021-06-29 10:35:42 +0100

    Choose rules via CLI arguments

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
+let rules = process.argv.slice(2).map(x => parseInt(x));
+const allRules = [3, 5, 7, 11, 13, 17];
+if (rules.filter(x => !allRules.includes(x)).length) throw("Invalid CLI argument");
+if (!rules.length) rules = allRules;
+
 const readline = require('readline').createInterface({
 	input: process.stdin,
 	output: process.stdout
@@ -8,20 +13,20 @@ readline.question("Enter maximum number: ", (value: string) => {
 
 	let maxN;
 	maxN = parseInt(value);
-	if (!maxN && maxN !== 0) console.log("Invalid input");
+	if (!maxN && maxN !== 0) console.error("Invalid input");
 	else {
 		for (let i = 1; i <= maxN; i++ ) {
 			let out: string[] = [];
-			if (i % 3 == 0) out.push("Fizz");
-			if (i % 5 == 0) out.push("Buzz");
-			if (i % 7 == 0) out.push("Bang");
-			if (i % 11 == 0) out = ["Bong"];
-			if (i % 13 == 0) {
+			if (rules.includes(3) && i % 3 == 0) out.push("Fizz");
+			if (rules.includes(5) && i % 5 == 0) out.push("Buzz");
+			if (rules.includes(7) && i % 7 == 0) out.push("Bang");
+			if (rules.includes(11) && i % 11 == 0) out = ["Bong"];
+			if (rules.includes(13) && i % 13 == 0) {
 				let j = out.findIndex(s => s[0] === "B");
 				if (j === -1) j = out.length;
 				out.splice(j, 0, "Fezz");
 			}
-			if (i % 17 == 0) out.reverse();
+			if (rules.includes(17) && i % 17 == 0) out.reverse();
 
 			if (out.length) console.log(out.join(""));
 			else console.log(i);
