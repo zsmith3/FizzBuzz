f09d46ff6928a2d027bbbde2950813d5d22defbd
Zak Smith <zsmith3.dev@gmail.com>
2021-06-29 09:26:22 +0100

    Print "Bong" on multiples of 11

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ for (let i = 1; i <= 100; i++ ) {
 	if (i % 3 == 0) out += "Fizz";
 	if (i % 5 == 0) out += "Buzz";
 	if (i % 7 == 0) out += "Bang";
+	if (i % 11 == 0) out = "Bong";
+
 	if (out) console.log(out);
 	else console.log(i);
 }
