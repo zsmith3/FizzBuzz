cf5bb7772d762409e238238746b645185ab709fb
Zak Smith <zsmith3.dev@gmail.com>
2021-06-29 15:09:59 +0100

    Neaten code, add new rules

--- index.ts
+++ index.ts
@@ -1,35 +1,58 @@
-let rules = process.argv.slice(2).map(x => parseInt(x));
-const allRules = [3, 5, 7, 11, 13, 17];
-if (rules.filter(x => !allRules.includes(x)).length) throw("Invalid CLI argument");
-if (!rules.length) rules = allRules;
+import * as readline from 'readline';
 
-const readline = require('readline').createInterface({
+
+function validateRules(cliArgs: number[], validRules: number[]) {
+	if (cliArgs.some(x => !validRules.includes(x))) throw("Invalid CLI argument");
+	if (cliArgs.length) return cliArgs;
+	else return validRules;
+}
+
+const cliArgs = process.argv.slice(2).map(x => parseInt(x));
+const validRules = [3, 5, 7, 11, 13, 17, 19, 23];
+const userRules = validateRules(cliArgs, validRules);
+
+
+function fizzBuzz(num: number, rules: number[]): string {
+	let out: string[] = [];
+	if (rules.includes(3) && num % 3 == 0) out.push("Fizz");
+	if (rules.includes(5) && num % 5 == 0) out.push("Buzz");
+	if (rules.includes(7) && num % 7 == 0) out.push("Bang");
+	if (rules.includes(11) && num % 11 == 0) out = ["Bong"];
+	if (rules.includes(13) && num % 13 == 0) {
+		let bWordIndex = out.findIndex(word => word[0] === "B");
+		if (bWordIndex === -1) bWordIndex = out.length;
+		out.splice(bWordIndex, 0, "Fezz");
+	}
+	if (rules.includes(17) && num % 17 == 0) out.reverse();
+	if (rules.includes(19) && num % 19 == 0) {
+		for (let i = 0; i < out.length; i++) {
+			out[i] = out[i].slice(0, out[i].length - 1);
+		}
+	}
+	if (rules.includes(23) && num % 23 == 0) {
+		for (let i = 0; i < out.length; i++) {
+			if (out[i][0] === "F") out[i] += "e";
+			else out[i] += "o";
+		}
+	}
+
+	if (out.length) return out.join("");
+	else return num.toString();
+}
+
+
+const readLineInterface = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
 });
 
-readline.question("Enter maximum number: ", (value: string) => {
-	readline.close();
+readLineInterface.question("Enter maximum number: ", value => {
+	readLineInterface.close();
 
-	let maxN;
-	maxN = parseInt(value);
-	if (!maxN && maxN !== 0) console.error("Invalid input");
-	else {
-		for (let i = 1; i <= maxN; i++ ) {
-			let out: string[] = [];
-			if (rules.includes(3) && i % 3 == 0) out.push("Fizz");
-			if (rules.includes(5) && i % 5 == 0) out.push("Buzz");
-			if (rules.includes(7) && i % 7 == 0) out.push("Bang");
-			if (rules.includes(11) && i % 11 == 0) out = ["Bong"];
-			if (rules.includes(13) && i % 13 == 0) {
-				let j = out.findIndex(s => s[0] === "B");
-				if (j === -1) j = out.length;
-				out.splice(j, 0, "Fezz");
-			}
-			if (rules.includes(17) && i % 17 == 0) out.reverse();
+	const maxNumber = parseInt(value);
+	if (isNaN(maxNumber)) throw("Invalid input");
 
-			if (out.length) console.log(out.join(""));
-			else console.log(i);
-		}
+	for (let i = 1; i <= maxNumber; i++) {
+		console.log(fizzBuzz(i, userRules));
 	}
 });
